206c5b004b376ca459d5020e22cbd799ec651a3e
revisitor <mikado.brave@gmail.com>
2022-12-29 13:38:10 +0700

    Converted Gradle files to KTS

--- forecaster-core/build.gradle.kts
+++ forecaster-core/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id 'java-library'
-    id 'org.jetbrains.kotlin.jvm'
+    `java-library`
+    id("org.jetbrains.kotlin.jvm")
 }
 
 java {
@@ -9,5 +9,5 @@ java {
 }
 
 dependencies {
-    api 'org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4'
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
